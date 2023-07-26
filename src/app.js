34d10bbae321f3c44691aaabb8679aa3dedb2857
todeKim <wnstlrrla1@naverc.com>
2023-07-26 15:44:53 +0900

    first

---
+++ src/app.js
@@ -0,0 +1,14 @@
+//module
+const express = require('express');
+const app = express();
+
+//routing
+const home = require("./routes/home")
+
+//web setting
+app.set("views","./views");
+app.set("view engine", "ejs");
+
+app.use("/",home);
+
+module.exports = app;
